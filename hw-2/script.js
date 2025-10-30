2533784902d894fe694b31d3b820416c29558e33
Mikhail Shesterov <shesterov87@icloud.com>
2025-10-30 05:48:35 +0300

     раccкомментировал js

--- hw-2/script.js
+++ hw-2/script.js
@@ -1,113 +1,137 @@
-//let a = 10;
-//alert(a);
-//let a = 20;
-//alert('20');
-//let year = 2007;
-//alert(year);
-//let js = "BrendanEich";
-//alert(js);
-//const a = 10;
-//const b = 2;
-//let sum = a + b;
-//alert(sum);
-//let difference = a - b;
-//alert(difference);
-//let product = a * b;
-//alert(product);
-//let quotient = a / b;
-//alert(quotient);
-//let result = 2 ** 5;
-//alert(result);
-//const a = 9;
-//const b = 2;
-//let remainder = 9 % 2;
-//alert(remainder);
-//let num = 1;
-//num += 5;
-//num -= 3;
-//num *= 7;
-//num /= 3;
-//num++;
-//num--;
-//alert(num);
-//let age = prompt("Сколько вам лет?");
-//alert(age);
-//const user = {
-// name: 'Иван',
-//age: '32',
-//isAdmin: true
-//}
-//let userName = prompt('Привет, ИМЯ!');
-//alert(userName);
+let a = 10;
+alert(a);
+
+
+let a = 20;
+alert('20');
+
+
+let year = 2007;
+alert(year);
+
+
+let js = "BrendanEich";
+alert(js);
+
+
+const a = 10;
+const b = 2;
+let sum = a + b;
+alert(sum);
+
+
+let difference = a - b;
+alert(difference);
+
+
+let product = a * b;
+alert(product);
+
+
+let quotient = a / b;
+alert(quotient);
+
+
+let result = 2 ** 5;
+alert(result);
+
+
+const a = 9;
+const b = 2;
+let remainder = 9 % 2;
+alert(remainder);
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+const user = {
+   name: 'Иван',
+   age: '32',
+   isAdmin: true
+}
+let userName = prompt('Привет, ИМЯ!');
+alert(userName);
 
 
 
-//let password = 'пароль';
+let password = 'пароль';
 
-//let userInput = prompt("Введите пароль");
+let userInput = prompt("Введите пароль");
 
-//if (userInput === password) {
-//alert('Пароль введён верно');
-//} else {
-//alert('Пароль введён неправильно');
-//}
-
-
-//let c = 10;
-//let number = prompt('Введите число');
-//if (c >= 0 && c <= 10) {
-//console.log('Верно');
-//alert('Верно');
-//} else {
-//console.log('Неверно');
-//alert('Неверно');
-//}
-
-//let c = 0;
-//let number = prompt('Введите число');
-//if (c > 0 && c < 10) {
-//console.log('Верно');
-//alert('Верно');
-//} else {
-//console.log('Неверно');
-//alert('Неверно');
-//}
-
-//let c = -3;
-//let number = prompt('Введите число');
-//if (c > 0 && c < 10) {
-//console.log('Верно');
-//alert('Верно');
-//} else {
-//console.log('Неверно');
-//alert('Неверно');
-//}
-
-//let c = 0;
-//let number = prompt('Введите число');
-//if (c > 0 && c < 10) {
-//console.log('Верно');
-//alert('Верно');
-//} else {
-//console.log('Неверно');
-//alert('Неверно');
-//}
+if (userInput === password) {
+   alert('Пароль введён верно');
+} else {
+   alert('Пароль введён неправильно');
+}
 
 
-//let d = 20;
-//let e = 150;
+let c = 10;
+let number = prompt('Введите число');
+if (c >= 0 && c <= 10) {
+   console.log('Верно');
+   alert('Верно');
+} else {
+   console.log('Неверно');
+   alert('Неверно');
+}
 
-//if (d > 100 || e > 100) {
-   //console.log('Верно');
-//} else {
-   //console.log('Неверно');
-//}
+let c = 0;
+let number = prompt('Введите число');
+if (c > 0 && c < 10) {
+   console.log('Верно');
+   alert('Верно');
+} else {
+   console.log('Неверно');
+   alert('Неверно');
+}
+
+let c = -3;
+let number = prompt('Введите число');
+if (c > 0 && c < 10) {
+   console.log('Верно');
+   alert('Верно');
+
+} else {
+   console.log('Неверно');
+   alert('Неверно');
+
+}
+
+let c = 0;
+let number = prompt('Введите число');
+if (c > 0 && c < 10) {
+   console.log('Верно');
+   alert('Верно');
+} else {
+   console.log('Неверно');
+   alert('Неверно');
+}
 
 
-//let a = '2';
-//let b = '3';
+let d = 20;
+let e = 150;
 
-//alert(Number(a) + Number(b));
+if (d > 100 || e > 100) {
+   console.log('Верно');
+} else {
+   console.log('Неверно');
+}
+
+
+let a = '2';
+let b = '3';
+
+alert(Number(a) + Number(b));
 
 
 var monthNumber = prompt("Введите номер месяца");
@@ -137,7 +161,7 @@ switch (monthNumber) {
    case 'август':
       console.log('восьмой месяц');
       break;
-      case 'сентябрь':
+   case 'сентябрь':
       console.log('девятый месяц');
       break;
    case 'октябрь':
@@ -156,29 +180,29 @@ switch (monthNumber) {
 
 let hi = 0;
 while (hi < 2) {
-    console.log("привет");
-    hi++;
+   console.log("привет");
+   hi++;
 }
 
 
 for (let i = 1; i <= 5; i++) {
-    console.log(i);
+   console.log(i);
 }
 
 
 for (let i = 7; i <= 22; i++) {
-    console.log(i);
+   console.log(i);
 }
 
 const obj = {
-    "Коля": '200',
-    "Вася": '300', 
-    "Петя": '400'
+   "Коля": '200',
+   "Вася": '300',
+   "Петя": '400'
 };
 
 
 for (let name in obj) {
-    console.log(name + " — зарплата " + obj[name] + " долларов");
+   console.log(name + " — зарплата " + obj[name] + " долларов");
 }
 
 let n = 1000;
@@ -186,7 +210,7 @@ let num = 0;
 
 
 for (; n >= 50; num++) {
-    n = n / 2;
+   n = n / 2;
 }
 
 console.log("Получившееся число: " + n);
@@ -197,7 +221,7 @@ const firstFriday = 5;
 const daysInMonth = 31;
 
 for (let day = firstFriday; day <= daysInMonth; day += 7) {
-    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+   console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
 }
 
 
